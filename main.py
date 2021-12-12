c06f69b7316cdc733e2497b92d773083a212feaf
ReCir0 <93223617+ReCir0@users.noreply.github.com>
2021-12-12 19:13:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 """
 ITERTOOLS IMPLEMENTATION
 """
+
+
+
+
+def combinations_with_replacement(string_of_elements, amount_of_el):
+    #Implomenta an analogue of itertools combinations with replacement
+    index_list = [0] * amount_of_el
+    list_of_elements = tuple(string_of_elements)
+    edge_of_elements = len(list_of_elements) - 1
+    yield tuple(list_of_elements[i] for i in index_list)
+    while index_list != [edge_of_elements] * amount_of_el:
+        for index in range(amount_of_el - 1, -1, -1):
+            if index_list[index] != edge_of_elements: break
+        index_list[index:] = [index_list[index] + 1] * (amount_of_el - index)
+        yield tuple(list_of_elements[i] for i in index_list)
