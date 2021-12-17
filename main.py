380aeca96409c81eb5c6102471be2be0c90b0f61
shnasta <shvets.nasta@gmail.com>
2021-12-17 11:16:45 +0200

    changed place

--- main.py
+++ main.py
@@ -28,20 +28,6 @@ def repeat(value):
     while True:
         yield value
 
-
-
-def combinations_with_replacement(string_of_elements, amount_of_el):
-    #Imploments an analogue of itertools combinations with replacement
-    index_list = [0] * amount_of_el
-    list_of_elements = tuple(string_of_elements)
-    edge_of_elements = len(list_of_elements) - 1
-    yield tuple(list_of_elements[i] for i in index_list)
-    while index_list != [edge_of_elements] * amount_of_el:
-        for index in range(amount_of_el - 1, -1, -1):
-            if index_list[index] != edge_of_elements: break
-        index_list[index:] = [index_list[index] + 1] * (amount_of_el - index)
-        yield tuple(list_of_elements[i] for i in index_list)
-
  
 
 def product(*args, repeat=1):
@@ -85,6 +71,8 @@ def product(*args, repeat=1):
     for elem4 in new_lst:
         yield tuple(elem4)
 
+
+
 def combinations(iterable, r):
     """return generator with all combinations
     of r elemnets from iterable"""
@@ -106,3 +94,17 @@ def combinations(iterable, r):
         for j in range(i+1, r):
             ourange[j] = ourange[j-1] + 1
         yield tuple(input_it[i] for i in ourange)
+
+
+
+def combinations_with_replacement(string_of_elements, amount_of_el):
+    #Imploments an analogue of itertools combinations with replacement
+    index_list = [0] * amount_of_el
+    list_of_elements = tuple(string_of_elements)
+    edge_of_elements = len(list_of_elements) - 1
+    yield tuple(list_of_elements[i] for i in index_list)
+    while index_list != [edge_of_elements] * amount_of_el:
+        for index in range(amount_of_el - 1, -1, -1):
+            if index_list[index] != edge_of_elements: break
+        index_list[index:] = [index_list[index] + 1] * (amount_of_el - index)
+        yield tuple(list_of_elements[i] for i in index_list)
