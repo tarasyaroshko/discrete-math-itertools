f5d84f9fb620497c919fb8805b2518ed25830045
shnasta <shvets.nasta@gmail.com>
2021-12-21 11:12:55 +0200

    .

--- main.py
+++ main.py
@@ -75,7 +75,36 @@ def product(*args, repeat=1):
 
 def combinations(r, n):
     """return generator with all combinations
-    of r elemnets from range(n)"""
+    of r elemnets from range(n)
+    >>> for i in combinations(3,4):
+    ...     print(i)
+    (0, 1, 2)
+    (0, 1, 3)
+    (0, 2, 3)
+    (1, 2, 3)
+    >>> for i in combinations(5,7):
+    ...     print(i)
+    (0, 1, 2, 3, 4)
+    (0, 1, 2, 3, 5)
+    (0, 1, 2, 3, 6)
+    (0, 1, 2, 4, 5)
+    (0, 1, 2, 4, 6)
+    (0, 1, 2, 5, 6)
+    (0, 1, 3, 4, 5)
+    (0, 1, 3, 4, 6)
+    (0, 1, 3, 5, 6)
+    (0, 1, 4, 5, 6)
+    (0, 2, 3, 4, 5)
+    (0, 2, 3, 4, 6)
+    (0, 2, 3, 5, 6)
+    (0, 2, 4, 5, 6)
+    (0, 3, 4, 5, 6)
+    (1, 2, 3, 4, 5)
+    (1, 2, 3, 4, 6)
+    (1, 2, 3, 5, 6)
+    (1, 2, 4, 5, 6)
+    (1, 3, 4, 5, 6)
+    (2, 3, 4, 5, 6)"""
     if r>n:
         return
     ourange=[i for i in range(r)]
